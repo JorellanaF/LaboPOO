47914ad11d57af222c3a1f7c2a61be254c0f6d4b
jorge <00103717@uca.edu.sv>
2018-05-24 20:04:16 -0600

    Operacion

---
+++ src/Productos_Abstractos/Operacion.java
@@ -0,0 +1,12 @@
+/*
+Lo minimo que debe realizar
+ */
+package Productos_Abstractos;
+
+/**
+ *
+ * @author Jorge Orellana <00103717@uca.edu.sv>
+ */
+public interface Operacion {
+    double operar(double a,double b);
+}
